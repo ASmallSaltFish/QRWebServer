5a4df0595ecedac630aae3f58721a18bcfe34186
xuzhangsheng <xzs1017@163.com>
2018-11-20 16:38:51 +0800

    二维码生成接口

---
+++ src/test/java/com/huateng/test/qrService/DistingQrGenerateTest.java
@@ -0,0 +1,38 @@
+package com.huateng.test.qrService;
+
+import com.huateng.qrcode.base.parser.param.RequestVo;
+import com.huateng.qrcode.base.parser.param.base.AppParamHeader;
+import com.huateng.qrcode.base.parser.param.base.BusParamBody;
+import com.huateng.qrcode.base.parser.param.base.SysParamHeader;
+import com.huateng.qrcode.common.enums.ServiceConfigEnums;
+import com.huateng.qrcode.qrserver.QrServerManager;
+import com.huateng.qrcode.qrserver.impl.DistingQrGenerateImpl;
+import com.huateng.qrcode.utils.SpringContextUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class DistingQrGenerateTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void testServiceProvider() throws Exception {
+        Class<?> serviceClass = ServiceConfigEnums.getByServiceCode("003");
+        QrServerManager qrServerManager = (QrServerManager) applicationContext.getBean(serviceClass);
+        RequestVo requestVo = new RequestVo();
+        SysParamHeader sysParamHeader = new SysParamHeader();
+        AppParamHeader appParamHeader = new AppParamHeader();
+        BusParamBody busParamBody = new BusParamBody();
+        requestVo.setSysHeader(sysParamHeader);
+        requestVo.setAppHeader(appParamHeader);
+        requestVo.setBusBody(busParamBody);
+        qrServerManager.handler(requestVo);
+    }
+}
